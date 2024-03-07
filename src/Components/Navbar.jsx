c99e5f96178268c4b8daf8cd8439fe3a1ec75b5d
ASHISH-JHA94 <ajha94023@gmail.com>
2024-03-07 19:28:41 +0530

    making Navbar responsive for small devices

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,14 @@ import A from '../assets/Images/userAsset/A.png'
 
 export default function Navbar(){
     return(
-        <header className='flex w-full p-4 md:p-8 align-middle justify-between'>
-            <div className="logo flex flex-row items-baseline gap-2">
-                <img src={A} className='w-10 ' alt="#" />
-                <h2 className="logoText text-3xl m-[-8px]">Shish Jha</h2>
+        <header className='flex w-full p-3 md:p-8 align-middle justify-between'>
+            <div className="logo flex flex-row items-baseline md:gap-2 ml-0 gap-1">
+                <img src={A} className='md:w-10 w-8' alt="#" />
+                <h2 className="logoText md:text-3xl text-2xl m-[-8px]">Shish Jha</h2>
             </div>
 
             <nav className="flex">
-                <ul className='flex flex-row gap-4'>
+                <ul className='flex flex-row md:gap-4 gap-3 text-xl'>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#contactMe">Contact Me</a></li>
