4a56b8b5960d32296ff351e8e06ef393c0e09792
ASHISH-JHA94 <ajha94023@gmail.com>
2024-03-15 00:18:24 +0530

    project card responsive

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -9,7 +9,7 @@ const ProjectCard = ({ number, direction, title, description, skills, background
     <div className={`relative projectCard ${direction === 'left' ? 'leftNumber' : 'rightNumber'} bg-cover bg-center`} style={{ backgroundImage: `url(${backgroundImage})` }}>
 
 <div className="absolute top-10 right-10 text-white text-6xl md:text-9xl font-semibold hidden z-10 transition duration-800 projectNumber">{number}</div>
-      <div className="projectContent">
+      <div className="projectContent md:left-20 top-10">
         <div className="projectSkillsContainer  flex flex-wrap justify-start gap-3">
           {skills.map((skill, index) => (
             <img
@@ -23,7 +23,7 @@ const ProjectCard = ({ number, direction, title, description, skills, background
         <h2 className="projectHeading">{title}</h2>
         <p className="projectSubHeading">{description}</p>
         <div className="btnGroup">
-          <button className="btn btnProject">Read More</button>
+          <button className="btn btnProject p-1 md:p-4">Read More</button>
           <IconButton
             href="https://github.com/ASHISH-JHA94/Wanderlust.git"
             target="_blank"
