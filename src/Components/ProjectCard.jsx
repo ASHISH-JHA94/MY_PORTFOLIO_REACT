3ef8c6d53c1771c0682f775cd5c1333af9b006fc
ASHISH-JHA94 <ajha94023@gmail.com>
2024-06-29 15:09:33 +0530

    modified

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -15,9 +15,9 @@ const ProjectCard = ({
 }) => {
   return (
     <div
-      className={`relative projectCard ${
-        direction === "left" ? "leftNumber" : "rightNumber"
-      } bg-cover bg-center`}
+    className={`relative projectCard bg-cover bg-center ${
+      direction === "left" ? "leftNumber" : "rightNumber"
+    }`}
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="absolute top-10 right-10 text-white text-6xl md:text-9xl font-semibold hidden z-10 transition duration-800 projectNumber">
--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -93,7 +93,7 @@ const ProjectsSection = () => {
   return (
     <section id="projects" className="projectSection">
       <h2 className="projectTitle mb-12 text-6xl">Projects</h2>
-      <div className="projectContainer">
+      <div className="projectContainer mx-6">
         {projects.map((project) => (
           <ProjectCard key={project.id} {...project} />
         ))}
