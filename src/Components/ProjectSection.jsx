6b6892399254b57889d36dccf5042f8708d87fe7
ASHISH-JHA94 <ajha94023@gmail.com>
2024-06-29 15:33:44 +0530

    changing project

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -92,7 +92,7 @@ const ProjectsSection = () => {
 
   return (
     <section id="projects" className="projectSection">
-      <h2 className="projectTitle mb-12 text-6xl">Projects</h2>
+      <h2 className="projectTitle mb-4 text-6xl">Projects</h2>
       <div className="projectContainer mx-6">
         {projects.map((project) => (
           <ProjectCard key={project.id} {...project} />
